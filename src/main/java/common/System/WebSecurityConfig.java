8717ef30bca518d033edcdae0d1be9158d5eb19b
root <zarmir>
2019-02-22 04:50:36 -0500

    fix(bug encoding password)

--- src/main/java/common/System/WebSecurityConfig.java
+++ src/main/java/common/System/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package common.System;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;    
 import org.springframework.security.config.annotation.web.configuration.*;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.*;    
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;  
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;    
 @EnableWebSecurity    
@@ -19,51 +21,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       .username("admin").password("123").roles("ADMIN").build());    
       return manager;    
   }  */
+	  @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder auth) 
+	      throws Exception {
+		  auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance())
+          .withUser("user").password("123").roles("USER").and()
+          .withUser("admin").password("123").roles("USER", "ADMIN");
+	    }
 	
-	  @Override
-	   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	      auth.inMemoryAuthentication()
-	      .withUser("admin").password("123").roles("USER");
-	   }
-
 	  
   @Override    
   protected void configure(HttpSecurity http) throws Exception {    
-      http.authorizeRequests().anyRequest()
-      .hasAnyRole("USER","ADMIN")
-      .and()
-      .authorizeRequests().antMatchers("/login**")
-      .permitAll()
+	  http    .authorizeRequests()
+      //.antMatchers("/pages/**", "/registration", "/app.html").permitAll()
+	  .antMatchers("/","/static/**","/pages/**").permitAll()
+	  .anyRequest().authenticated()
       .and()
       .formLogin()
-      .loginPage("/login")
-      .usernameParameter("userid")  // Username parameter, used in name attribute
-      // of the <input> tag. Default is 'username'.
-.passwordParameter("passwd")  // Password parameter, used in name attribute
-      // of the <input> tag. Default is 'password'.
-.successHandler((req,res,auth)->{    //Success handler invoked after successful authentication
-for (GrantedAuthority authority : auth.getAuthorities()) {
-System.out.println(authority.getAuthority());
-}
-System.out.println(auth.getName());
-res.sendRedirect("/"); // Redirect user to index/home page
-});
-      
-      
-      
-      
-    /*  
-      antMatchers("/index", "/user","/").permitAll()  
-      .antMatchers("/admin","/status","/productos").authenticated()  
-      .and()  
-      .formLogin() 
-      .and()  
-      .logout() .permitAll() 
-      
-      
-      
-      
-      .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));*/
+      //.loginPage("/login")
+      .permitAll()
+      .and()
+      .logout()
+      .permitAll();
+	  
+	
         
   }    
 }    
